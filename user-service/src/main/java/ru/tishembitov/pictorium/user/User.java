28e13ab44cad48a510d0ea4294b7f86f69dc2b1f
tishembitov <tishembitov@gmail.com>
2025-10-02 21:40:35 +0500

    feat: add user entity

---
+++ user-service/src/main/java/ru/tishembitov/pictorium/user/User.java
@@ -0,0 +1,67 @@
+package ru.tishembitov.pictorium.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String keycloakId;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+//    @Column(unique = true, nullable = false)
+//    private String email;
+
+    private String image;
+
+    private String bannerImage;
+
+    private String description;
+
+    private String chatColor = "blue";
+
+    private Integer chatSize = 384;
+
+    private Boolean sideOpen = true;
+
+    private Long selectedBoard;
+
+    private Instant recommendationCreatedAt;
+
+    private String instagram;
+
+    private String tiktok;
+
+    private String telegram;
+
+    private String pinterest;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+}
