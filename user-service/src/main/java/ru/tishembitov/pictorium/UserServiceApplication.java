5e185fdc4a6f47eed55ddfdead734b448c080b6c
tishembitov <tishembitov@gmail.com>
2025-10-03 18:42:44 +0500

    feat: add annotation for main application

--- user-service/src/main/java/ru/tishembitov/pictorium/UserServiceApplication.java
+++ user-service/src/main/java/ru/tishembitov/pictorium/UserServiceApplication.java
@@ -2,12 +2,13 @@ package ru.tishembitov.pictorium;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
-
 }
