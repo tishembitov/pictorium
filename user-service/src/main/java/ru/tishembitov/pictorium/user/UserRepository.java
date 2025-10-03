1950f6da980b82d3ffd833da39ee7a89971f6373
tishembitov <tishembitov@gmail.com>
2025-10-03 18:35:27 +0500

    feat: create user repository

---
+++ user-service/src/main/java/ru/tishembitov/pictorium/user/UserRepository.java
@@ -0,0 +1,19 @@
+package ru.tishembitov.pictorium.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByKeycloakId(String keycloakId);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByKeycloakId(String keycloakId);
+}
