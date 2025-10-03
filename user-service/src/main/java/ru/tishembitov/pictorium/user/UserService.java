341d2e28ba15bbdeaa6dc036d848c28db3e1ae18
tishembitov <tishembitov@gmail.com>
2025-10-03 18:36:29 +0500

    feat: update user service interface

--- user-service/src/main/java/ru/tishembitov/pictorium/user/UserService.java
+++ user-service/src/main/java/ru/tishembitov/pictorium/user/UserService.java
@@ -1,19 +1,22 @@
 package ru.tishembitov.pictorium.user;
 
 import jakarta.validation.Valid;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
 public interface UserService {
-    UserResponseDto getUserByKeycloakId(String keycloakId);
+
+    UserResponseDto getCurrentUser(Jwt jwt);
 
     UserResponseDto getUserById(UUID id);
 
     UserResponseDto getUserByUsername(String username);
 
-    UserResponseDto updateUser(String subject, @Valid UserUpdateDto patchRequest);
+    UserResponseDto updateUser(Jwt jwt, @Valid UserUpdateDto userUpdateDto);
 
     UserResponseDto uploadBannerImage(UUID id, MultipartFile file);
 
+    UserResponseDto uploadProfileImage(UUID id, MultipartFile file);
 }
