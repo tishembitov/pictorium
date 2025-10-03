0693245b230e3bc92d321f8a8a5d9c94a9da3a46
tishembitov <tishembitov@gmail.com>
2025-10-03 18:38:40 +0500

    refactor: update file service interface

--- user-service/src/main/java/ru/tishembitov/pictorium/file/FileStorageService.java
+++ user-service/src/main/java/ru/tishembitov/pictorium/file/FileStorageService.java
@@ -1,7 +1,14 @@
 package ru.tishembitov.pictorium.file;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import java.nio.file.Path;
 
 public interface FileStorageService {
     Path getFile(String filePath);
+
+    void deleteFile(String image);
+
+    String saveFile(MultipartFile file, String image);
+
 }
