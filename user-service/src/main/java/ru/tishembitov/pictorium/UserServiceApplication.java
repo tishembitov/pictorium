0f9534b9397886c762fb1c82e6359531f84688a8
tishembitov <tishembitov@gmail.com>
2025-10-04 11:53:51 +0500

    refactor: add jpa auditing annotation

--- user-service/src/main/java/ru/tishembitov/pictorium/UserServiceApplication.java
+++ user-service/src/main/java/ru/tishembitov/pictorium/UserServiceApplication.java
@@ -3,8 +3,10 @@ package ru.tishembitov.pictorium;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @EnableFeignClients
 public class UserServiceApplication {
 
