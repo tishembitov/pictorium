fdd10444495d0598af12c4df3de0a720dc888785
tishembitov <tishembitov@gmail.com>
2025-10-10 12:41:32 +0500

    feat: create Subscription entity

---
+++ user-service/src/main/java/ru/tishembitov/pictorium/subscription/Subscription.java
@@ -0,0 +1,39 @@
+package ru.tishembitov.pictorium.subscription;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import ru.tishembitov.pictorium.user.User;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "subscriptions",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"follower_id", "following_id"}))
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_id", nullable = false)
+    private User follower;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "following_id", nullable = false)
+    private User following;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+}
