5716fb692677474c99e9e4bcf8dbd782dbc4cdb8
tishembitov <tishembitov@gmail.com>
2025-10-13 00:18:29 +0500

    refactor: remove previous dto's

--- user-service/src/main/java/ru/tishembitov/pictorium/subscription/FollowCheckResponseDto.java
+++
@@ -1,4 +0,0 @@
-package ru.tishembitov.pictorium.subscription;
-
-public record FollowCheckResponseDto(boolean isFollowing) {
-}
--- user-service/src/main/java/ru/tishembitov/pictorium/subscription/SubscriptionResponseDto.java
+++
@@ -1,4 +0,0 @@
-package ru.tishembitov.pictorium.subscription;
-
-public record SubscriptionResponseDto(String status) {
-}
