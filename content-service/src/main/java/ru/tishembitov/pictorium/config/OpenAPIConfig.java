dbe7db622ea4bf2fa6792a08ed852d1c260d03a6
tishembitov <tishembitov@gmail.com>
2025-10-13 14:54:01 +0500

    feat: add security and openapi configs

---
+++ content-service/src/main/java/ru/tishembitov/pictorium/config/OpenAPIConfig.java
@@ -0,0 +1,19 @@
+package ru.tishembitov.pictorium.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Content Service")
+                        .version("1.0")
+                        .description("OpenApi documentation for content-service"));
+    }
+}
