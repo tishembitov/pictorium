b2dd1434faef20c5a1f68bd7100444f1939373cf
tishembitov <tishembitov@gmail.com>
2025-10-13 14:52:22 +0500

    refactor: remove invalid fields for request dto's

--- content-service/src/main/java/ru/tishembitov/pictorium/pin/PinCreateRequest.java
+++ content-service/src/main/java/ru/tishembitov/pictorium/pin/PinCreateRequest.java
@@ -11,9 +11,6 @@ public record PinCreateRequest(
         @Size(max = 200) String title,
         @Size(max = 400) String description,
         @Size(max = 200) String href,
-        @Size(max = 200) String image,
-        @Size(max = 200) String videoPreview,
-        @Size(max = 100) String rgb,
         @Size(max = 100) String height,
         Set<@NotBlank @Size(max = 100) String> tags
 ) {}
