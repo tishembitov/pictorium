a7e35253819107874a5dc331cb7f8d1db269fcc7
tishembitov <tishembitov@gmail.com>
2025-10-18 19:48:14 +0500

    refactor: add URL annotation

--- content-service/src/main/java/ru/tishembitov/pictorium/pin/PinCreateRequest.java
+++ content-service/src/main/java/ru/tishembitov/pictorium/pin/PinCreateRequest.java
@@ -3,6 +3,7 @@ package ru.tishembitov.pictorium.pin;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.URL;
 
 import java.util.Set;
 
@@ -11,8 +12,8 @@ public record PinCreateRequest(
         @Size(max = 200) String title,
         @Size(max = 400) String description,
         @Size(max = 200) String href,
-        @Size(max = 200) String imageUrl,
-        @Size(max = 200) String videoPreviewUrl,
+        @URL String imageUrl,
+        @URL String videoPreviewUrl,
         @Size(max = 100) String rgb,
         @Size(max = 100) String height,
         Set<@NotBlank @Size(max = 100) String> tags
--- content-service/src/main/java/ru/tishembitov/pictorium/pin/PinUpdateRequest.java
+++ content-service/src/main/java/ru/tishembitov/pictorium/pin/PinUpdateRequest.java
@@ -2,14 +2,15 @@ package ru.tishembitov.pictorium.pin;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.URL;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record PinUpdateRequest(
         @Size(max = 200) String title,
         @Size(max = 400) String description,
         @Size(max = 200) String href,
-        @Size(max = 200) String imageUrl,
-        @Size(max = 200) String videoPreviewUrl,
+        @URL String imageUrl,
+        @URL String videoPreviewUrl,
         @Size(max = 100) String rgb,
         @Size(max = 100) String height
 ) {}
