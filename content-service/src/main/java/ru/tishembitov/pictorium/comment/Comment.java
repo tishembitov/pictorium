cea9915264c16754048285f5be238ec17dd93fdc
tishembitov <tishembitov@gmail.com>
2025-10-20 19:50:20 +0500

    refactor: remove replies and like collections

--- content-service/src/main/java/ru/tishembitov/pictorium/comment/Comment.java
+++ content-service/src/main/java/ru/tishembitov/pictorium/comment/Comment.java
@@ -5,12 +5,9 @@ import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import ru.tishembitov.pictorium.like.Like;
 import ru.tishembitov.pictorium.pin.Pin;
 
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -37,14 +34,6 @@ public class Comment {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "comment_id")
     private Comment parentComment;
-
-    @OneToMany(mappedBy = "parentComment", cascade = CascadeType.ALL, orphanRemoval = true)
-    @Builder.Default
-    private Set<Comment> replies = new HashSet<>();
-
-    @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL, orphanRemoval = true)
-    @Builder.Default
-    private Set<Like> likes = new HashSet<>();
 
     @Column(nullable = false)
     @Builder.Default
