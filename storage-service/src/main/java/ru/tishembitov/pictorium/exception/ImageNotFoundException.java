e50dd26a0e5b4fd3a859daeab26c48e1e37a2c34
tishembitov <tishembitov@gmail.com>
2025-11-06 22:49:43 +0500

    feat: add additional files of storage service

---
+++ storage-service/src/main/java/ru/tishembitov/pictorium/exception/ImageNotFoundException.java
@@ -0,0 +1,8 @@
+package ru.tishembitov.pictorium.exception;
+
+public class ImageNotFoundException extends RuntimeException {
+
+    public ImageNotFoundException(String message) {
+        super(message);
+    }
+}
