0b4d40567cf8273583bcbea309a95ba67163f3e7
tishembitov <tishembitov@gmail.com>
2026-01-11 15:07:48 +0500

    feat: refactor allowed swagger api route

--- chat-service/src/main/java/ru/tishembitov/pictorium/config/SecurityConfig.java
+++ chat-service/src/main/java/ru/tishembitov/pictorium/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/**").permitAll()
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/*/v3/api-docs/**").permitAll()
                         .requestMatchers("/ws/**").permitAll() // WebSocket handshake
                         .anyRequest().authenticated()
                 )
