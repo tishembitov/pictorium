2e94f78534cf7de641ae5533f48bf38f1f048873
tishembitov <tishembitov@gmail.com>
2026-01-11 14:49:40 +0500

    feat: add gradle files and test

---
+++ chat-service/src/main/java/ru/tishembitov/pictorium/ChatServiceApplication.java
@@ -0,0 +1,16 @@
+package ru.tishembitov.pictorium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableJpaAuditing
+public class ChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatServiceApplication.class, args);
+	}
+}
--- notification-service/src/main/java/ru/tishembitov/pictorium/kafka/event/ContentEvent.java
+++ notification-service/src/main/java/ru/tishembitov/pictorium/kafka/event/ContentEvent.java
@@ -1,14 +1,12 @@
 package ru.tishembitov.pictorium.kafka.event;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
